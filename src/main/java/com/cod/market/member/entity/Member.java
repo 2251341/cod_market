627133a46cfaf6481cf3e01427e85400bfa1919a
2251341 <dydwksos246@naver.com>
2024-07-10 15:24:04 +0900

    add entity

--- src/main/java/com/cod/market/member/entity/Member.java
+++ src/main/java/com/cod/market/member/entity/Member.java
@@ -1,9 +1,12 @@
 package com.cod.market.member.entity;
 
 import com.cod.market.market.base.BaseEntity;
+import com.cod.market.question.entity.Question;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,5 +16,7 @@ public class Member extends BaseEntity {
     private String password;
     private String nickname;
     private String email;
+    private String isActive;
+    private List<Question> questionList;
 
 }
--- src/main/java/com/cod/market/product/entity/Product.java
+++ src/main/java/com/cod/market/product/entity/Product.java
@@ -1,9 +1,13 @@
 package com.cod.market.product.entity;
 
 import com.cod.market.market.base.BaseEntity;
+import com.cod.market.market.entity.Market;
+import com.cod.market.question.entity.Question;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,6 +15,11 @@ import lombok.Setter;
 public class Product extends BaseEntity {
 
     private String name;
+    private String description;
+    private String isActive;
     private int price;
+    private int hitCount;
 
+    private Market market;
+    private List<Question> questionList;
 }

78e2d22fa6ad240b9ec423671f498df03a223d65
2251341 <dydwksos246@naver.com>
2024-07-16 15:39:59 +0900

    admin privilege set

--- src/main/java/com/cod/market/home/AdmHomeController.java
+++ src/main/java/com/cod/market/home/AdmHomeController.java
@@ -2,23 +2,26 @@ package com.cod.market.home;
 
 import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("adm")
+@RequestMapping("/adm")
 @RequiredArgsConstructor
 public class AdmHomeController {
     private final ProductService productService;
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String index(Model model) {
         return "redirect:/adm/home/main";
     }
 
     @GetMapping("/home/main")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showMain() {
         return "adm/home/main";
     }

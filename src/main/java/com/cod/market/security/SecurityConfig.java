36a85734c2da7c850ede3b0e5951d1bf055e6479
2251341 <dydwksos246@naver.com>
2024-07-16 14:40:21 +0900

    add member devinit data

--- src/main/java/com/cod/market/security/SecurityConfig.java
+++ src/main/java/com/cod/market/security/SecurityConfig.java
@@ -56,4 +56,5 @@ public class SecurityConfig {
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
 }
